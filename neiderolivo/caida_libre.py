9e92277382788e74a48227ae7d18f0c4fb2035b3
neider olivo <neiderolivo3@gmail.com>
2020-03-03 08:43:23 -0500

    trabajos

---
+++ neiderolivo/caida_libre.py
@@ -0,0 +1,7 @@
+print('caida libre')
+yo=float(input('ingese el valor de la altura inicial:'))
+g=float(input('ingrese el valor de la gravedad:'))
+t=float(input('ingrese el valor del tiempo:'))
+y=yo-0.5*g*t**2
+
+print('el valor de la altura a la cual se encontara el objeto es:',y)
